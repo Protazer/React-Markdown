10779eb56d1c9f075525090038ff0d5fc626619c
florian <florian.huberdeau@laposte.net>
2021-07-25 18:31:59 +0200

    feat(markdown): feat markdown state and onchange event close #2

--- src/App.js
+++ src/App.js
@@ -5,22 +5,31 @@ import "./App.css";
 import { sampleText } from "./sampleText";
 
 class App extends Component {
+  state = {
+    text: sampleText,
+  };
+
+  handleChange = (e) => {
+    const text = e.target.value;
+    this.setState({ text });
+  };
   render() {
     return (
       <div className="container pt-3">
         <section className="row markdown">
           <article className="col-12 order-2 col-md-6 order-md-1 markdown__content">
             <textarea
+              onChange={this.handleChange}
               className="form-control"
               name="content"
               id="content"
               rows="35"
-              value={sampleText}
+              value={this.state.text}
             ></textarea>
           </article>
           <article className="col-12 order-1 col-md-6 order-md-2  markdown__result">
             <h1 className="markdown__result__Title">Résultat</h1>
-            <div className="markdown__result__content">{sampleText}</div>
+            <div className="markdown__result__content">{this.state.text}</div>
           </article>
         </section>
       </div>
